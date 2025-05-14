2a3f6942daf114ed6c4fb7b50dbc09907e6c2d78
Sabina Balaeva <sabibalaeva@gmail.com>
2025-05-14 22:34:15 +0200

    update port for hosting platform

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { app } from "./src/app.js"
 
-app.listen(3000, () => {
-  console.log("Server listening on http://localhost:3000")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
 })
